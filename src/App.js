089c166a6f7ff3c59de5240728e4a08a0f40cb97
Rounak6611 <r19cs423@cit.reva.edu.in>
2022-05-08 21:21:27 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import Users from "./Components/cards";
+import "./App.css";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    // Set initial state
+    this.state = { users_data: [], loading: false };
+
+    this.updateState = this.updateState.bind(this);
+  }
+
+  updateState() {
+    this.setState({ loading: true });
+
+    setTimeout(
+      async function () {
+        const response = await fetch("https://reqres.in/api/users?page=1");
+        const jsonresponse = await response.json();
+        console.log(jsonresponse);
+        this.setState({ users_data: jsonresponse["data"], loading: false });
+      }.bind(this),
+      2000
+    );
+  }
+
+  render() {
+    return (
+      <>
+        <nav className="navbar">
+          <div className="navitem">
+            <p class="text">My Users</p>
+            
+            <button className="fetchbt" onClick={this.updateState}>
+            <b>GET USERS</b>
+            </button>
+          </div>
+        </nav>
+        <div className="userdatacontainer">
+          <Users loading={this.state.loading} users={this.state.users_data} />
+        </div>
+        <div class="about-cotainer">
+          <h3>About Lets Grow More</h3>
+          <p className="about">Let's Learn Together, Let's "Grow Together"
+Our goal is to help others to gain personal and professional skills in the area of Technological development. The purpose is to provide a structure through which students may exercise their right to freely associate in pursuit of a common purpose or goal that enhances community at LetsGrowMore.</p>
+         
+            
+
+          
+        </div>
+        
+      </>
+    );
+  }
 }
 
 export default App;
